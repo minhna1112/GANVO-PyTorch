3233f7671614ea1373b19b4d2d685482f675122c
tuanndhust <nguyentuan2701hd@gmail.com>
2022-03-14 23:56:31 +0700

    lossesssssss

--- models/losses.py
+++ models/losses.py
@@ -1,2 +1,133 @@
 import torch
+from torch import nn
+import torch.nn.functional as F
+from inverse_wrap import inverse_warp
+from __future__ import division
+import numpy as np
+from discriminator import Discriminator
+from generator import Generator
+
+#sfmlearner with 1 scale
+def photometric_reconstruction_loss(tgt_img, ref_imgs, intrinsics,
+                                    depth, pose,
+                                    rotation_mode='euler', padding_mode='zeros'):
+
+    assert(pose.size(1) == len(ref_imgs))
+
+    reconstruction_loss = 0
+    b, _, h, w = depth.size()
+    downscale = tgt_img.size(2)/h
+
+    tgt_img_scaled = F.interpolate(tgt_img, (h, w), mode='area')
+    ref_imgs_scaled = [F.interpolate(ref_img, (h, w), mode='area') for ref_img in ref_imgs]
+    intrinsics_scaled = torch.cat((intrinsics[:, 0:2]/downscale, intrinsics[:, 2:]), dim=1)
+
+    warped_imgs = []
+    diff_maps = []
+
+    for i, ref_img in enumerate(ref_imgs_scaled):
+        current_pose = pose[:, i]
+
+        ref_img_warped, valid_points = inverse_warp(ref_img, depth[:,0], current_pose,
+                                                        intrinsics_scaled,
+                                                        rotation_mode, padding_mode)
+        diff = (tgt_img_scaled - ref_img_warped) * valid_points.unsqueeze(1).float()
+
+
+
+        reconstruction_loss += diff.abs().mean()
+        assert((reconstruction_loss == reconstruction_loss).item() == 1)
+
+        warped_imgs.append(ref_img_warped[0])
+        diff_maps.append(diff[0])
+
+    return reconstruction_loss, warped_imgs, diff_maps
+
+#reading the pytorch tutorial, reading GAN...
+def gan_optimize(netG, netD):
+    #create D and G
+    netG = Generator(ngpu).to(device)
+    netD = Discriminator(ngpu).to(device)
+    # Initialize BCELoss function
+    criterion = nn.BCELoss()
+
+    # Create batch of latent vectors that we will use to visualize
+    #  the progression of the generator
+    fixed_noise = torch.randn(64, nz, 1, 1, device=device)
+    #setup Adam optimizers for both G and D
+    optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
+    optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
+    return
+
+
+#sfmlearner
+
+@torch.no_grad()
+def compute_depth_errors(gt, pred, crop=True):
+    abs_diff, abs_rel, sq_rel, a1, a2, a3 = 0,0,0,0,0,0
+    batch_size = gt.size(0)
+
+    '''
+    crop used by Garg ECCV16 to reprocude Eigen NIPS14 results
+    construct a mask of False values, with the same size as target
+    and then set to True values inside the crop
+    '''
+    if crop:
+        crop_mask = gt[0] != gt[0]
+        y1,y2 = int(0.40810811 * gt.size(1)), int(0.99189189 * gt.size(1))
+        x1,x2 = int(0.03594771 * gt.size(2)), int(0.96405229 * gt.size(2))
+        crop_mask[y1:y2,x1:x2] = 1
+    skipped = 0
+    for current_gt, current_pred in zip(gt, pred):
+        valid = (current_gt > 0) & (current_gt < 80)
+        if crop:
+            valid = valid & crop_mask
+        if valid.sum() == 0:
+            continue
+
+        valid_gt = current_gt[valid]
+        valid_pred = current_pred[valid].clamp(1e-3, 80)
+
+        valid_pred = valid_pred * torch.median(valid_gt)/torch.median(valid_pred)
+
+        thresh = torch.max((valid_gt / valid_pred), (valid_pred / valid_gt))
+        a1 += (thresh < 1.25).float().mean()
+        a2 += (thresh < 1.25 ** 2).float().mean()
+        a3 += (thresh < 1.25 ** 3).float().mean()
+
+        abs_diff += torch.mean(torch.abs(valid_gt - valid_pred))
+        abs_rel += torch.mean(torch.abs(valid_gt - valid_pred) / valid_gt)
+
+        sq_rel += torch.mean(((valid_gt - valid_pred)**2) / valid_gt)
+    if skipped == batch_size:
+        return None
+
+    return [metric.item() / (batch_size - skipped) for metric in [abs_diff, abs_rel, sq_rel, a1, a2, a3]]
+
+
+@torch.no_grad()
+def compute_pose_errors(gt, pred):
+    RE = 0
+    for (current_gt, current_pred) in zip(gt, pred):
+        snippet_length = current_gt.shape[0]
+        scale_factor = torch.sum(current_gt[..., -1] * current_pred[..., -1]) / torch.sum(current_pred[..., -1] ** 2)
+        ATE = torch.norm((current_gt[..., -1] - scale_factor * current_pred[..., -1]).reshape(-1)).cpu().numpy()
+        R = current_gt[..., :3] @ current_pred[..., :3].transpose(-2, -1)
+        for gt_pose, pred_pose in zip(current_gt, current_pred):
+            # Residual matrix to which we compute angle's sin and cos
+            R = (gt_pose[:, :3] @ torch.inverse(pred_pose[:, :3])).cpu().numpy()
+            s = np.linalg.norm([R[0, 1]-R[1, 0],
+                                R[1, 2]-R[2, 1],
+                                R[0, 2]-R[2, 0]])
+            c = np.trace(R) - 1
+            # Note: we actually compute double of cos and sin, but arctan2 is invariant to scale
+            RE += np.arctan2(s, c)
+
+    return [ATE/snippet_length, RE/snippet_length]
+
+
+
+
+
+
 
