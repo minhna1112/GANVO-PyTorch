1d3968655c1f1048d1a5d1327ca382fcb792c319
ivsr <ivsr.set.hust@gmail.com>
2022-03-14 23:13:37 +0700

    data and dataloader tested

--- datasets/sequence_folders.py
+++ datasets/sequence_folders.py
@@ -1,9 +1,14 @@
+import torch
 import torch.utils.data as data
 import numpy as np
 from imageio import imread
 from path import Path
 import random
 
+import argparse
+import custom_transforms
+
+from tqdm import tqdm
 
 def load_as_float(path):
     return imread(path).astype(np.float32)
@@ -32,21 +37,21 @@ class SequenceFolder(data.Dataset):
 
     def crawl_folders(self, sequence_length):
         sequence_set = []
-        demi_length = (sequence_length-1)//2
-        shifts = list(range(-demi_length, demi_length + 1))
-        shifts.pop(demi_length)
-        for scene in self.scenes:
-            intrinsics = np.genfromtxt(scene/'cam.txt').astype(np.float32).reshape((3, 3))
-            imgs = sorted(scene.files('*.jpg'))
+        demi_length = (sequence_length-1)//2  #1
+        shifts = list(range(-demi_length, demi_length + 1)) #[-1, 0, 1]
+        shifts.pop(demi_length)  #[-1, 1]
+        for scene in self.scenes: #iterate through each folder
+            intrinsics = np.genfromtxt(scene/'cam.txt').astype(np.float32).reshape((3, 3)) #K matrix
+            imgs = sorted(scene.files('*.jpg')) #List of filenames in each folder
             if len(imgs) < sequence_length:
                 continue
-            for i in range(demi_length, len(imgs)-demi_length):
+            for i in range(demi_length, len(imgs)-demi_length): #(1, n-1)
                 sample = {'intrinsics': intrinsics, 'tgt': imgs[i], 'ref_imgs': []}
-                for j in shifts:
+                for j in shifts: #im(i+1], im(i-1)
                     sample['ref_imgs'].append(imgs[i+j])
                 sequence_set.append(sample)
         random.shuffle(sequence_set)
-        self.samples = sequence_set
+        self.samples = sequence_set  #List of dict
 
     def __getitem__(self, index):
         sample = self.samples[index]
@@ -58,7 +63,51 @@ class SequenceFolder(data.Dataset):
             ref_imgs = imgs[1:]
         else:
             intrinsics = np.copy(sample['intrinsics'])
-        return tgt_img, ref_imgs, intrinsics, np.linalg.inv(intrinsics)
+        return tgt_img, ref_imgs, intrinsics, np.linalg.inv(intrinsics) #[I(t), [I(t-1, I(t+1)], K, K^-1
 
     def __len__(self):
         return len(self.samples)
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='GANVO training on KITTI Dataset',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('data', metavar='DIR',
+                    help='path to dataset')
+    parser.add_argument('--sequence-length', type=int, metavar='N', help='sequence length for training', default=3)
+    parser.add_argument('--seed', default=0, type=int, help='seed for random functions, and network initialization')
+    parser.add_argument('-b', '--batch-size', default=8, type=int,
+                    metavar='N', help='mini-batch size')    
+    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers')
+
+
+    args = parser.parse_args()
+
+    # Data loading code
+    normalize = custom_transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                                            std=[0.5, 0.5, 0.5])
+    train_transform = custom_transforms.Compose([
+        custom_transforms.RandomHorizontalFlip(),
+        custom_transforms.RandomScaleCrop(),
+        custom_transforms.ArrayToTensor(),
+        normalize
+    ])
+
+    train_set = SequenceFolder(
+        args.data,
+        transform=train_transform,
+        seed=args.seed,
+        train=True,
+        sequence_length=args.sequence_length
+    )
+
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=args.batch_size, shuffle=True,
+        num_workers=args.workers, pin_memory=True)
+
+
+    for i, (tgt_img, ref_imgs, intrinsics, intrinsics_inv) in enumerate(tqdm(train_loader)):
+        continue
+
+    # valid_transform = custom_transforms.Compose([custom_transforms.ArrayToTensor(), normalize])
